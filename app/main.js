2e1f2b8346e020ed7bf4ee93eea32f553565386c
Ryan Faerman <ryan@faerman.net>
2016-02-10 20:35:38 -0500

    appy app app

--- app/main.js
+++ app/main.js
@@ -23,7 +23,7 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
-  child = spawn('../goop');
+  child = spawn('./goop');
 
   // Create the browser window.
   mainWindow = new BrowserWindow({
